242037aefd99224472d9a35aa8090f94ff947046
pragashraj <rajpragashraj30@gmail.com>
2021-08-10 10:33:33 +0530

    test30

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.repository.CustomerRepository;
+import com.example.demo.request.CreateNewCustomerRequest;
+import com.example.demo.usecases.CreateCustomerUseCase;
 import com.example.demo.usecases.WelcomeUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -26,6 +26,16 @@ public class TestController {
         try {
             WelcomeUseCase useCase = new WelcomeUseCase(customerRepository);
             return useCase.execute();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<?> create(@RequestBody CreateNewCustomerRequest request) {
+        try {
+            CreateCustomerUseCase useCase = new CreateCustomerUseCase(customerRepository, request);
+            return ResponseEntity.ok(useCase.execute());
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
