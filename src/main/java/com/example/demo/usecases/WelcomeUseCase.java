1e36717f65038c4180c0484ad7526e0fcdcc9d07
pragashraj <rajpragashraj30@gmail.com>
2021-08-31 10:01:13 +0530

    test34

--- src/main/java/com/example/demo/usecases/WelcomeUseCase.java
+++ src/main/java/com/example/demo/usecases/WelcomeUseCase.java
@@ -1,14 +1,6 @@
 package com.example.demo.usecases;
 
-import com.example.demo.repository.CustomerRepository;
-
 public class WelcomeUseCase {
-    private final CustomerRepository customerRepository;
-
-    public WelcomeUseCase(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
     public String execute() {
         return "Welcome";
     }
